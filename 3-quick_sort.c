31241e56c713ede5f9256c7a865dc3bd6bfb9efb
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-24 15:18:47 -0700

    3-.. starting

---
+++ 3-quick_sort.c
@@ -0,0 +1,7 @@
+#include "sort.h"
+
+/**
+ * quick_sort - quick sorts an array of int
+ * @array: the array to sort ascendingly
+ * @size: the size of the array
+ */
--- sort.h
+++ sort.h
@@ -21,7 +21,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-void swap_node(listint_t **h, listint_t *node1, listint_t *node2);
+void swap(int* a, int* b);
+int lomuto_partition(int arr[], int low, int high);
+
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
