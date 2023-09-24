5ea0103b2a0f285e9555f0bb56dcb5266b94fc18
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-25 00:02:53 +0100

    sort.h re-updating

--- sort.h
+++ sort.h
@@ -21,9 +21,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-void swap(int *a, int *b, int *array, size_t size);
-int lomuto_partition(int arr[], int low, int high, int *array, size_t size);
-void quick_sort_recur(int arr[], int low, int high, int *array, size_t size);
+int lomuto_partition(int *array, int start, int end, int size)
+void quick_sort_recur(int *array, int start, int end, int size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
