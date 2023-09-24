fadcd3fbc05ef77c98a9f5a5e34d2ababb708d4c
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-24 09:49:02 -0700

    0-bubble_sort.c created

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sorts an array of int (assending)
+ * @array: the array of ints
+ * @size: the size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp, counter = 0;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 2; i++)
+	{
+		counter = 0;
+		for (j = 0; j < size - 2; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				counter++;
+				print_array(array, size);
+			}
+		}
+		if (counter == 0)
+			break;
+	}
+}
