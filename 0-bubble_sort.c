5b2b8043f62305d91aa968bdf9554e805cacc9c8
HalimaEla59 <halimaelabbadi@gmail.com>
2023-09-24 10:01:40 -0700

    fix2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,15 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp, counter = 0;
-	size_t i, j;
+	size_t i, j, size2 = size;
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size - 1; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		counter = 0;
-		for (j = 0; j < size - 2; j++)
+		for (j = 0; j < size2 - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -29,5 +30,6 @@ void bubble_sort(int *array, size_t size)
 		}
 		if (counter == 0)
 			break;
+		size2--;
 	}
 }
