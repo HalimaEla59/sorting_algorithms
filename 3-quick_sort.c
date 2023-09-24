217866a3868f597ff37294fdc9f73e39048ab858
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-09-24 23:21:20 +0100

    3-quick_sort.c continuing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,44 @@
 #include "sort.h"
 
+
+void swap(int* a, int* b, int *array, size_t size) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+    
+    // Print array after swap
+    print_array(array, size);
+}
+
+int lomuto_partition(int arr[], int low, int high, int *array, size_t size) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j], array, size);
+        }
+    }
+    swap(&arr[i + 1], &arr[high], array, size);
+    return i + 1;
+}
+
+void quick_sort_recursive(int arr[], int low, int high, int *array, size_t size) {
+    if (low < high) {
+        int pi = lomuto_partition(arr, low, high, array, size);
+
+        quick_sort_recursive(arr, low, pi - 1, array, size);
+        quick_sort_recursive(arr, pi + 1, high, array, size);
+    }
+}
+
 /**
  * quick_sort - quick sorts an array of int
  * @array: the array to sort ascendingly
  * @size: the size of the array
  */
+
+void quick_sort(int *array, size_t size) {
+    quick_sort_recursive(array, 0, size - 1, array, size);
+}
